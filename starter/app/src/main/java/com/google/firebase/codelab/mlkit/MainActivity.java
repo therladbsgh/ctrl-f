e6c7ef2162d3e5dc1232df674ddc63eef4141315
Peter <pbgcho@gmail.com>
2018-10-20 19:36:15 -0400

    Somewhat working camera

---
+++ starter/app/src/main/java/com/google/firebase/codelab/mlkit/MainActivity.java
@@ -0,0 +1,378 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.codelab.mlkit;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Pair;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.Manifest;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.codelab.mlkit.GraphicOverlay.Graphic;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = "MainActivity";
+    private ImageView mImageView;
+    private Button mButton;
+    private Button mCloudButton;
+    private Bitmap mSelectedImage;
+    private GraphicOverlay mGraphicOverlay;
+    // Max width (portrait mode)
+    private Integer mImageMaxWidth;
+    // Max height (portrait mode)
+    private Integer mImageMaxHeight;
+
+    private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder holder;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_CODE_CAMERA_PERMISSION = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.image_view);
+        surfaceView =  findViewById(R.id.surface_view);
+
+        mButton = findViewById(R.id.button_text);
+        mCloudButton = findViewById(R.id.button_cloud_text);
+        requestNeededPermission();
+
+        mGraphicOverlay = findViewById(R.id.graphic_overlay);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runTextRecognition();
+            }
+        });
+        mCloudButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraIntent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+
+                }
+            }
+        });
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"Image 1", "Image 2", "Image 3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout
+                .simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap bmp = (Bitmap) extras.get("data");
+        }
+    }
+
+    public void requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                Toast.makeText(MainActivity.this, "I need Camera", Toast.LENGTH_SHORT).show();
+            }
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CODE_CAMERA_PERMISSION);
+        } else {
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_CAMERA_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(MainActivity.this, "CAMERA perm granted", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "CAMERA perm NOT granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void runTextRecognition() {
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+            FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                    .getOnDeviceTextRecognizer();
+            mButton.setEnabled(false);
+            detector.processImage(image)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<FirebaseVisionText>() {
+                                @Override
+                                public void onSuccess(FirebaseVisionText texts) {
+                                    mButton.setEnabled(true);
+                                    processTextRecognitionResult(texts);
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Task failed with an exception
+                                    mButton.setEnabled(true);
+                                    e.printStackTrace();
+                                }
+                            });
+
+        }
+
+
+//    private void processTextRecognitionResult(FirebaseVisionText texts) {
+//        // TODO: Add your code here to process the on-device text recognition results.
+//    }
+        private void processTextRecognitionResult(FirebaseVisionText texts) {
+            List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+            if (blocks.size() == 0) {
+                showToast("No text found");
+                return;
+            }
+            mGraphicOverlay.clear();
+            for (int i = 0; i < blocks.size(); i++) {
+                List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+                for (int j = 0; j < lines.size(); j++) {
+                    List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                    for (int k = 0; k < elements.size(); k++) {
+                        Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+                        mGraphicOverlay.add(textGraphic);
+
+                    }
+                }
+            }
+
+        }
+
+
+    private void runCloudTextRecognition() {
+        // TODO: Add your code here to run cloud text recognition.
+    }
+
+    private void processCloudTextRecognitionResult(FirebaseVisionDocumentText text) {
+        // TODO: Add your code here to process the cloud text recognition results.
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Functions for loading images from app assets.
+
+    // Returns max image width, always for portrait mode. Caller needs to swap width / height for
+    // landscape mode.
+    private Integer getImageMaxWidth() {
+        if (mImageMaxWidth == null) {
+            // Calculate the max width in portrait mode. This is done lazily since we need to
+            // wait for
+            // a UI layout pass to get the right values. So delay it to first time image
+            // rendering time.
+            mImageMaxWidth = mImageView.getWidth();
+        }
+
+        return mImageMaxWidth;
+    }
+
+    // Returns max image height, always for portrait mode. Caller needs to swap width / height for
+    // landscape mode.
+    private Integer getImageMaxHeight() {
+        if (mImageMaxHeight == null) {
+            // Calculate the max width in portrait mode. This is done lazily since we need to
+            // wait for
+            // a UI layout pass to get the right values. So delay it to first time image
+            // rendering time.
+            mImageMaxHeight =
+                    mImageView.getHeight();
+        }
+
+        return mImageMaxHeight;
+    }
+
+    // Gets the targeted width / height.
+    private Pair<Integer, Integer> getTargetedWidthHeight() {
+        int targetWidth;
+        int targetHeight;
+        int maxWidthForPortraitMode = getImageMaxWidth();
+        int maxHeightForPortraitMode = getImageMaxHeight();
+        targetWidth = maxWidthForPortraitMode;
+        targetHeight = maxHeightForPortraitMode;
+        return new Pair<>(targetWidth, targetHeight);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        mGraphicOverlay.clear();
+        switch (position) {
+            case 0:
+                mSelectedImage = getBitmapFromAsset(this, "Please_walk_on_the_grass.jpg");
+
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                mSelectedImage = getBitmapFromAsset(this, "non-latin.jpg");
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                mSelectedImage = getBitmapFromAsset(this, "nl2.jpg");
+                break;
+        }
+        if (mSelectedImage != null) {
+            // Get the dimensions of the View
+            Pair<Integer, Integer> targetedSize = getTargetedWidthHeight();
+
+            int targetWidth = targetedSize.first;
+            int maxHeight = targetedSize.second;
+
+            // Determine how much to scale down the image
+            float scaleFactor =
+                    Math.max(
+                            (float) mSelectedImage.getWidth() / (float) targetWidth,
+                            (float) mSelectedImage.getHeight() / (float) maxHeight);
+
+            Bitmap resizedBitmap =
+                    Bitmap.createScaledBitmap(
+                            mSelectedImage,
+                            (int) (mSelectedImage.getWidth() / scaleFactor),
+                            (int) (mSelectedImage.getHeight() / scaleFactor),
+                            true);
+
+            mImageView.setImageBitmap(resizedBitmap);
+            mSelectedImage = resizedBitmap;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Do nothing
+    }
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream is;
+        Bitmap bitmap = null;
+        try {
+            is = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//
+//        camera = Camera.open();
+//        Camera.Parameters parameters = camera.getParameters();
+//        parameters.setPictureFormat(ImageFormat.JPEG);
+//        camera.setParameters(parameters);
+//
+//
+//        camera.startPreview();
+//        holder = surfaceView.getHolder();
+//        holder.addCallback(this);
+//        camera.setPreviewCallback(new Camera.PreviewCallback() {
+//            public void onPreviewFrame(byte[] data, Camera camera) {
+//                // You could apply some pixel operations directly here.
+//                Log.d("Camera", "Camera image aquired");
+//            }
+//        });
+//    }
+//    @Override
+//    public void surfaceCreated(SurfaceHolder holder) {
+//        try {
+//            camera.setPreviewDisplay(holder);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        camera.startPreview();
+//
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        if (camera != null) {
+//            camera.stopPreview();
+//            camera.release();
+//            camera = null;
+//        }
+//    }
+//
+//    @Override
+//    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+//    }
+//
+//    @Override
+//    public void surfaceDestroyed(SurfaceHolder holder) {
+//    }
+
+
+
+
+}
