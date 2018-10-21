05e61fdf4a56d4b67798150803c5b8f1af2075ca
Peter <pbgcho@gmail.com>
2018-10-21 01:43:30 -0400

    reap used memory

---
+++ starter/app/src/main/java/com/google/firebase/codelab/mlkit/MyPreview.java
@@ -0,0 +1,15 @@
+package com.google.firebase.codelab.mlkit;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class MyPreview extends ViewGroup {
+    public MyPreview(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
